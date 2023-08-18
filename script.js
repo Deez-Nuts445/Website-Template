7e4f69d3ae2c9a661246a480d40145666ac81f7f
Deez-Nuts445 <deeznuts631101@gmail.com>
2023-08-18 08:20:33 +0800

    Removed all functions that open links in a new window.

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
 function a() {
-window.open("https://poe.com/LuaBot");
+console.log("Button pressed!")
 };
-function b() {
-window.open("https://www.buymeacoffee.com/deeznuts445");
-};
-function c() {
-    window.open("https://kick.com/DeezNuts455");
-}; 
-function d() {
-    window.open("https://user.rblx.name/herman_484")
-};
-function e() {
-   window.open("https://chng.it/4kbMYG5VqT") 
-};
-function f() {
-    window.open("https://is.gd/NRpCZF")
-}
-function g() {
-    window.open("https://deez-nuts445.github.io/redirectr.html")
-}
